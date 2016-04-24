9a75e3594bfe7be22d50125f28e377cd7ddb54ed
Stas Ukolov <ukoloff@gmail.com>
2016-04-24 13:32:00 +0500

    Borrow API

--- lib/appveyor/worker.rb
+++ lib/appveyor/worker.rb
@@ -1,5 +1,46 @@
+require 'json'
+require 'net/http'
+
 require_relative "worker/version"
 
 module AppVeyor::Worker
-    # Your code goes here...
+  def self.skip?
+    !api
+  end
+
+  def self.message msg, details=nil
+    x = api or return
+    body = JSON.generate category: 'info',
+      message: msg,
+      details: details
+    x.post '/api/build/messages',
+      body,
+      'Content-Length'=>body.length.to_s,
+      'Content-Type'=>'application/json'
+  end
+
+  def self.test info
+    x = api or return
+    body = JSON.generate info
+    x.post '/api/tests',
+      body,
+      'Content-Length'=>body.length.to_s,
+      'Content-Type'=>'application/json'
+  end
+
+  private
+
+  def self.api
+    return if false===@http
+    return @http if @http
+    unless z = ENV['APPVEYOR_API_URL']
+      @http = false
+      return
+    end
+    z = URI z
+    @http = x = Net::HTTP.start z.host, z.port
+    x.use_ssl='https'==z.scheme
+    x
+  end
+
 end
