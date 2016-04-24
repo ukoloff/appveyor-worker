36ea41e6c4d3c4eab5fd5880ab39a36567143adb
Stas Ukolov <ukoloff@gmail.com>
2016-04-24 13:49:17 +0500

    Add some tests

---
+++ test/meta.rb
@@ -0,0 +1,20 @@
+class TestMeta < Minitest::Test
+  Z = AppVeyor::Worker
+
+  def test_class
+    assert Z
+    assert Module===Z
+  end
+
+  def test_skip
+    assert Z.respond_to? :skip?
+  end
+
+  def test_methods
+    %i(message test env).each do |meth|
+      assert Z.respond_to? meth
+    end
+  end
+
+
+end
